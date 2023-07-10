2b9d61395a8e50360e144c2e6fe1b197dac60d02
[Moises Almeida] <[moisesssprg@gmail.com]>
2023-07-10 19:15:43 -0300

    method Post Category

--- backend/src/main/java/com/spring/backend/services/CategoryService.java
+++ backend/src/main/java/com/spring/backend/services/CategoryService.java
@@ -30,4 +30,12 @@ public class CategoryService {
 		
 		return new CategoryDTO(entity);
 	}
+
+	public CategoryDTO save(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = categoryRepository.save(entity);
+		
+		return new CategoryDTO(entity);
+	}
 }
